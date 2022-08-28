7c7dc2eadb4ad686468a5fe1ed3d4e984a0494d6
Petrov Nikolay <nikolay.petrov99@mail.ru>
2022-08-28 12:53:47 +0300

    zadanie1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+using static System.Console;
+
+int A = Number("A");
+int B = Number("B");
+WriteLine($"{A},{B} => {Exponentiation(A,B)}");
+//WriteLine($"{A},{B} => {Math.Pow(A,B)}");
+
+int Number(string name) {
+    Write("Введите число {name}: ");
+    return int.Parse(ReadLine());
+}
+
+int Exponentiation(int A, int B) {
+    int result = 1;
+    for(int i = 0; i < B; i++) {
+        result = result*A;
+    }
+    return result;
+}
